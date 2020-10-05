22b9de780de210bb0a2895c0e530440abfc36e24
malialundstrom <lundstro22@up.edu>
2020-10-04 17:06:44 -0700

    added some onClick stuff

--- app/src/main/java/edu/up/gamestatehearts/MainActivity.java
+++ app/src/main/java/edu/up/gamestatehearts/MainActivity.java
@@ -3,18 +3,34 @@ package edu.up.gamestatehearts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.Normalizer;
+
+import static edu.up.gamestatehearts.R.id.runTestButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editText;
+    private Button testButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        testButton = (Button) findViewById(R.id.runTestButton);
+        testButton.setOnClickListener(this);
         editText = findViewById(R.id.editTextMulti);
+    }
+
+    @Override
+    public void onClick(View v){
+        editText.setText("", TextView.BufferType.NORMAL);
+        gameStateHearts firstInstance= new gameStateHearts();
+
 
     }
+
 }
--- app/src/main/java/edu/up/gamestatehearts/gameStateHearts.java
+++ app/src/main/java/edu/up/gamestatehearts/gameStateHearts.java
@@ -39,4 +39,5 @@ public class gameStateHearts {
         heartsBroken = false;
         suitLed = coins;
     }
+
 }
