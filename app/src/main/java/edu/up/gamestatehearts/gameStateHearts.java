22ff421dea9f4ff4971189e00688592d2898822d
araaks <addisonraak@gmail.com>
2020-10-04 17:37:58 -0700

    Implemented the toString() method.

--- app/src/main/java/edu/up/gamestatehearts/gameStateHearts.java
+++ app/src/main/java/edu/up/gamestatehearts/gameStateHearts.java
@@ -1,6 +1,7 @@
 package edu.up.gamestatehearts;
 
-import java.util.ArrayList;
+import android.util.Log;
+import java.util.*;
 
 import static edu.up.gamestatehearts.Card.coins;
 
@@ -21,6 +22,10 @@ public class gameStateHearts {
     ArrayList<Card> p2Hand = new ArrayList<>();
     ArrayList<Card> p3Hand = new ArrayList<>();
     ArrayList<Card> p4Hand = new ArrayList<>();
+    String p1HandString;
+    String p2HandString;
+    String p3HandString;
+    String p4HandString;
     boolean heartsBroken;
     int suitLed;
 
@@ -38,5 +43,55 @@ public class gameStateHearts {
         numCards = 13;
         heartsBroken = false;
         suitLed = coins;
+    }
+
+    /**
+     * this method prints the values of all of the variables in the game state by saving them all
+     *  to a String.
+     */
+    @Override
+    public String toString() {
+
+        //sets the ArrayList to a String to be returned with the rest of the info
+        for(Card tempCard : p1Hand) {
+            p1HandString = "Suit: " + tempCard.cardSuit + "\tValue: " + tempCard.cardVal + "\n";
+        }
+
+        for(Card tempCard : p2Hand) {
+            p2HandString = "Suit: " + tempCard.cardSuit + "\tValue: " + tempCard.cardVal + "\n";
+        }
+
+        for(Card tempCard : p3Hand) {
+            p3HandString = "Suit: " + tempCard.cardSuit + "\tValue: " + tempCard.cardVal + "\n";
+        }
+
+        for(Card tempCard : p4Hand) {
+            p4HandString = "Suit: " + tempCard.cardSuit + "\tValue: " + tempCard.cardVal + "\n";
+        }
+
+
+        return //prints the CURRENT score of the players to the Logcat Info window
+                "Player 1 Current Points: " + p1numCurrentPoints
+                + "Player 2 Current Points: " + p2numCurrentPoints
+                + "Player 3 Current Points: " + p3numCurrentPoints
+                + "Player 4 Current Points: " + p4numCurrentPoints
+
+                //prints the RUNNING score of the players to the Logcat Info window
+                + "Player 1 Running Points: " + p1RunningPoints
+                + "Player 2 Running Points: " + p2RunningPoints
+                + "Player 3 Running Points: " + p3RunningPoints
+                + "Player 4 Running Points: " + p4RunningPoints
+
+                //prints numCards
+                + "Number of Cards in Hands: " + numCards
+
+                //prints reference for what the suit numbers mean
+                + "1 = Cups\n2 = swords\n3 = coins\n4 = wands"
+
+                //prints the hands of each player
+                + "Player 1 Hand:\n" + p1HandString + "\n"
+                + "Player 2 Hand:\n" + p2HandString + "\n"
+                + "Player 3 Hand:\n" + p3HandString + "\n"
+                + "Player 4 Hand:\n" + p4HandString;
     }
 }
